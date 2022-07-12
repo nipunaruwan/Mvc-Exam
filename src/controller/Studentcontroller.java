d7223481d1c5cae2ee380b2dfd16a518702f2a66
mipunaruwan <nipunakalpa1234@gmail.com>
2022-07-12 10:56:43 +0530

    encapsulation student model

--- src/controller/Studentcontroller.java
+++ src/controller/Studentcontroller.java
@@ -1,4 +1,27 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
 public class Studentcontroller {
+    public JFXTextField txtemail;
+    public JFXTextField txtsname;
+    public JFXTextField txtSudentID;
+    public JFXTextField txtcontact;
+    public JFXTextField txtaddress;
+    public JFXTextField txtnic;
+    public TableView tblstudent;
+
+    public void btnsave(ActionEvent actionEvent) {
+    }
+
+    public void btnupdate(ActionEvent actionEvent) {
+    }
+
+    public void btndelete(ActionEvent actionEvent) {
+    }
+
+    public void btnsearch(ActionEvent actionEvent) {
+    }
 }
